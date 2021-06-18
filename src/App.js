68a7cc97b84cef39a6b7999047665ed5c6896d55
Madhuri111 <lakshmi.madhuri@students.iiit.ac.in>
2021-06-18 11:50:41 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import StateHook from './Hooks/StateHook1';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <StateHook />
+    </div>
+  );
+}
+
+export default App;
